09e3c7477d8443c90c120f544101b96699e0f225
Omar Alonso Lopez Valenzuela <88732850+omarsinho@users.noreply.github.com>
2022-09-21 22:33:26 -0500

    Probando Pop-ups

---
+++ SaludOS-Movil/ViewControllerPruebaPopUp.swift
@@ -0,0 +1,23 @@
+//
+//  ViewControllerPruebaPopUp.swift
+//  SaludOS-Movil
+//
+//  Created by Invitado on 21/09/22.
+//
+
+import UIKit
+
+class ViewControllerPruebaPopUp: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+     @IBAction func cierraView(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+     }
+     
+}
