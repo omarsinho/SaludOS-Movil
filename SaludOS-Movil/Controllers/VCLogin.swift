912241f67e109e0055d2b8eb4c3fed083cafb1ae
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2022-09-24 23:38:25 -0500

    Restauración del proyecto, corrección de referencias, y Login implementado
    Debido a un gran problema con las referencias, se tuvo que volver a una versión antigua donde no existía este problema. A su vez también se hizo la implementación del Login y una separación de Storyboards.

--- SaludOS-Movil/Controllers/VCLogin.swift
+++ SaludOS-Movil/Controllers/VCLogin.swift
@@ -14,7 +14,8 @@ class VCLogin: UIViewController {
     
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfPassword: UITextField!
-    @IBOutlet weak var btnIniciaSesion: UIButton!
+    
+    @IBOutlet weak var btnIniciarSesion: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,100 +27,49 @@ class VCLogin: UIViewController {
         //checkUserInfo()
     }
     
-    //MARK: - Navigation
-    
-    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "Login" {
-            // Verifica que el campo de texto no esté vacío.
-            if let email = tfEmail.text,
-               let password = tfPassword.text {
-                Auth.auth().signIn(withEmail: email, password: password) {
-                    (result, error) in
-                    
-                    if let result = result, error == nil {
-                        print("Hola")
-                        self.dismiss(animated: true)
-                    }
-                    else {
-                        let alerta = UIAlertController(title: "Error", message: "Debe haber texto en el campo", preferredStyle: .alert)
-                        let accion = UIAlertAction(title: "OK", style: .cancel)
-                        alerta.addAction(accion)
-                        self.present(alerta, animated: true)
-                    }
-                }
-            }
-            
-        }
-    }*/
+     // MARK: - Navigation
     
     @IBAction func IniciarSesion(_ sender: UIButton) {
         print("Pulsé el botón")
         validarTextFields()
-        /*// Validate Text Fields
-        
-        // Create cleaned versions of the text field
-        /*let email = tfEmail.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-         let password = tfPassword.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-         
-         // Iniciando sesión
-         Auth.auth().signIn(withEmail: email, password: password) {
-         (result, error) in
-         }*/
-        if let email = tfEmail.text,
-           let password = tfPassword.text {
-            Auth.auth().signIn(withEmail: email, password: password) {
-                (result, error) in
-                
-                if let result = result, error == nil {
-                    return true
-                }
-                else {
-                    return false
-                }
-            }
-        }
-        dismiss(animated: true)*/
     }
-    
+        
     func validarTextFields() {
         if tfEmail.text?.isEmpty == true {
             print("No email text")
             let alerta = UIAlertController(title: "Error", message: "Debe haber texto en el campo", preferredStyle: .alert)
             let accion = UIAlertAction(title: "OK", style: .cancel)
-            alerta.addAction(accion)
-            present(alerta, animated: true)
+                alerta.addAction(accion)
+                present(alerta, animated: true)
         }
         if tfPassword.text?.isEmpty == true {
-            print("No password")
+                print("No password")
             let alerta = UIAlertController(title: "Error", message: "Debe haber texto en el campo", preferredStyle: .alert)
             let accion = UIAlertAction(title: "OK", style: .cancel)
-            alerta.addAction(accion)
-            present(alerta, animated: true)
+                alerta.addAction(accion)
+                present(alerta, animated: true)
         }
-        
+            
         login()
     }
-    
+        
     func login() {
         Auth.auth().signIn(withEmail: tfEmail.text!, password: tfPassword.text!) {
-            [weak self] authResult, err in
-            guard let strongSelf = self else {return}
-                if let err = err {
-                    print(err.localizedDescription)
-                }
-            else {
-                print("Hola, sí funciona")
+            (result, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+                print("Hola y Adios Mundo")
+                let alerta = UIAlertController(title: "Error", message: error!.localizedDescription, preferredStyle: .alert)
+                let accion = UIAlertAction(title: "OK", style: .cancel)
+                    alerta.addAction(accion)
+                self.present(alerta, animated: true)
             }
-                //self!.checkUserInfo()
-        /*Auth.auth().currentUser != nil {
-            print(Auth.auth().currentUser?.uid)
-            let storyboard = UIStoryboard(name: "Inicio", bundle: nil)
-            let vc = storyboard.instantiateViewController(withIdentifier: "VCInicio")
-            vc.modalPresentationStyle = overFullScreen
-            present(vc, animated: true)*/
+            else {
+                self.performSegue(withIdentifier: "LoginAInicio", sender: self)
+            }
         }
     }
-    
+        
     func checkUserInfo() {
         if Auth.auth().currentUser != nil {
             print(Auth.auth().currentUser?.uid)
