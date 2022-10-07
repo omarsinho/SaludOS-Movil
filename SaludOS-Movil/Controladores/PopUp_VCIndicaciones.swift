48634eb3292040618c8c7cbe336990612dc9ec10
Omar Alonso Lopez Valenzuela <88732850+omarsinho@users.noreply.github.com>
2022-10-07 15:59:18 -0500

    Vista de Indicaciones de los médicos
    Se ajustó el stepper falta configurar cambio de pagina por medio del page control y hay un bug con el + del stepper se probará con otro dispositivo

--- SaludOS-Movil/Controladores/PopUp_VCIndicaciones.swift
+++ SaludOS-Movil/Controladores/PopUp_VCIndicaciones.swift
@@ -7,25 +7,54 @@
 
 import UIKit
 
-class PopUp_VCIndicaciones: UIViewController, UIScrollViewDelegate  {
+class PopUp_VCIndicaciones: UIViewController, UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var lbMedico: UILabel!
     
-    var cantPaginas = 6
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stpMedicos: UIStepper!
+    @IBOutlet weak var pageControlMedicos: UIPageControl!
+    @IBOutlet weak var textViewIndicaciones: UITextView!
     
-    @IBOutlet weak var pageControl: UIPageControl!
     
+    var listaMedicos = ["Pancho", "Uvaldo","Ignacio", "Simi", "Catafixio", "Masiosare"]
+    var listaIndicaciones = ["Hola", "Quién soy", "No sé", "Se me olvidó" , "Yeh, yeh", "Bad Bunny beibe"]
+    
+    var listaMedicinas = [String]() //Idea: Crear clase Medicina para incluir en el table view
     override func viewDidLoad() {
         super.viewDidLoad()
-        pageControl.numberOfPages = cantPaginas
+        pageControlMedicos.numberOfPages = listaMedicos.count
+        stpMedicos.maximumValue = Double(listaMedicos.count - 1)
     }
     
-
+    func cambiaDatos(pos:Int){
+        lbMedico.text = listaMedicos[pos]
+        textViewIndicaciones.text = listaIndicaciones[pos]
+        // listaMedicinas = se hace get de las medicinas que receta el Médico encargado
+        tableView.reloadData()
+    }
     
+    @IBAction func cambiaStepper(_ sender: UIStepper) {
+        cambiaDatos(pos: Int(stpMedicos.value))
+        pageControlMedicos.currentPage = Int(stpMedicos.value)
+    }
+    
+    @IBAction func cambiaPage(_ sender: UIPageControl) {
+        stpMedicos.value = Double(pageControlMedicos.currentPage)
+    }
     @IBAction func Regresar(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaMedicinas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celda")!
+        celda.textLabel?.text = listaMedicos[indexPath.row]
+        return celda
+    }
     
     /*
     // MARK: - Navigation
