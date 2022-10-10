b20bf60da5ccef64e77165bf1a142884bf462555
Sergio Carrera <75439050+Chimeroni@users.noreply.github.com>
2022-10-10 18:17:30 -0500

    Intento #1 para tener foto de perfil mediante Segue y ImagePicker

--- SaludOS-Movil/Controladores/PopUp_VCPrivacidad.swift
+++ SaludOS-Movil/Controladores/PopUp_VCPrivacidad.swift
@@ -24,16 +24,4 @@ class PopUp_VCPrivacidad: UIViewController {
     @IBAction func Regresar(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
